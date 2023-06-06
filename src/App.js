ab0b81be84a80f02c40848c7b8c017242182256a
unknown <gmlcjf1808@naver.com>
2023-06-06 23:47:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Homepage from './component/Homepage';
 import Aboutpage from './component/Aboutpage';
 import {Routes, Route} from 'react-router-dom';
 import NavBar from './component/NavBar';
+import Video from './component/Video';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Video />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutpage />} />
---
+++ src/component/Video.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className='main-img'>
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_2121556276/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_1590708592/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_720590472/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_767196300/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+
+        <div>
+            <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_358622757/imageComponent/image.imgs.full.high.jpg' />
+        </div>
+    </div>
+  )
+}
+
+export default Video
