35fa823ae9eb22edc87e34bcde180c7a90b1174c
unknown <gmlcjf1808@naver.com>
2023-06-28 00:40:29 +0900

    first commit

---
+++ src/component/Video.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div>
+        <div className="mask">
+
+            <div className="img_wrap">
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard/imageComponent/image.imgs.full.high.jpg" alt=""/>
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_2121556276/imageComponent/image.imgs.full.high.jpg" alt=""/>
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_1590708592/imageComponent/image.imgs.full.high.jpg" alt=""/>
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_720590472/imageComponent/image.imgs.full.high.jpg" alt=""/>
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_767196300/imageComponent/image.imgs.full.high.jpg" alt=""/>
+                <img src="https://www.ford.co.kr/content/ford/kr/ko_kr/site-wide-content/billboard-carousels/home-page/jcr:content/par/billboard_358622757/imageComponent/image.imgs.full.high.jpg" alt=""/>
+            </div>
+
+            <ul className="btn">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Video
