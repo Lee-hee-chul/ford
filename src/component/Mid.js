1c2e5c0f654f974dc63d889431d0110a42bd532e
unknown <gmlcjf1808@naver.com>
2023-10-31 23:19:06 +0900

    first commit

---
+++ src/component/Mid.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Mid = () => {
+  return (
+    <div>
+        <div className='mid-wrap'>
+            <div className='img-img'>
+                <img src='https://www.ford.co.kr/content/ford/kr/ko_kr/home/jcr:content/par/billboard_1608625802/imageComponent/image.imgs.full.high.jpg' />
+            </div>
+
+            <div className='mid-text'>
+              <h2 className='text-top'>FORD EXPLORER</h2>
+              <p className='text-center'>지금 바로 만나보세요</p>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Mid
